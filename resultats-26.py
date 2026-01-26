d9b8eb81276d69e9627cfb29a7832a49363ba4a0
mulobites <ricardhugas@hotmail.com>
2026-01-26 17:42:41 +0100

    Add files via upload

--- resultats-26.py
+++ resultats-26.py
@@ -1,10 +1,12 @@
-import paho.mqtt.client as paho
-from paho import mqtt
 import streamlit as st
 import json
 import pandas as pd
-import openpyxl as xl
+import paho.mqtt.client as paho
+from paho import mqtt
 
+
+
+st.header ("resultats 2026")
 
 with open("controls.json") as llistacontrols:
     llistacontrols=json.loads(llistacontrols.read())
@@ -12,12 +14,33 @@ with open("controls.json") as llistacontrols:
 with open("dorsals.json") as llistadorsals:
     llistadorsals=json.loads(llistadorsals.read())
 
+resultats={}
+for d in llistadorsals:
+    v=llistadorsals[d]
+    resultats[v]={}
+    for c in llistacontrols:
+       resultats[v][c]=""
 
-resultats=pd.DataFrame(index=llistadorsals.values(), columns=llistacontrols)
 
-st.write(resultats)
-st.write(resultats.count())
+with open("resultats.json") as llistaresultats:
+    resultats=json.loads(llistaresultats.read())
 
+
+contador={}
+for d in llistadorsals:
+    contador[d]=0
+
+espai=st.empty()
+
+
+
+respd=pd.DataFrame.from_dict(resultats).transpose()
+espai.table(resultats)
+espai.write("respd")
+espai.table(respd)
+
+espai.write("contador")
+espai.table(contador)
 
 def on_connect(client, userdata, flags, rc, blue):
     print("Connected")
@@ -28,14 +51,18 @@ def on_subscribe(client, userdata, mid, reasoncodes, properties):
     st.write("Subscribed")
 
 def on_message(client, userdata, msg):
+    espai.empty()
     triada=json.loads(msg.payload)
-    resultats.loc[triada[0], triada[1]]=triada[2]
-    st.write(resultats)
-    contador=list(resultats.count())
-    st.write(contador)
-    client.publish(topic="resultats", payload=json.dumps(contador), qos=1)
-
-
+    espai.table(triada)
+    resultats[triada[0]][triada[1]]=triada[2]
+    with open("resultats.json", "w") as llistaresultats:
+        json.dump(resultats, llistaresultats)
+    respd=pd.DataFrame.from_dict(resultats).transpose()
+    #respd.loc[triada[0], triada[1]]=triada[2]
+    espai.table(respd)
+    #contador=list(len(resultats))
+    #st.table(contador)
+    #client.publish(topic="resultats", payload=json.dumps(contador), qos=1)
 
 client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
 client.on_connect=on_connect
@@ -51,10 +78,6 @@ client.connect("fd483681811b46998f1720ee6faf6416.s1.eu.hivemq.cloud", 8883)
 
 
 client.subscribe("marxa", 1)
-
-
-st.header("resultats")
-
 
 
 client.loop_forever()
